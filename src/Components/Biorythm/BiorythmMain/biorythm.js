7605723ff9bc48db4dca7ff73450301fa151994b
MrFlavZz <flav621@gmail.com>
2020-06-06 00:42:30 +0200

    simplify calculus

--- src/Components/Biorythm/BiorythmMain/biorythm.js
+++ src/Components/Biorythm/BiorythmMain/biorythm.js
@@ -1,77 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import useStyles from "./style";
 import {Line} from 'react-chartjs-2';
-import classes from './style'
+import {annotation} from 'chartjs-plugin-annotation'
 import Grid from "@material-ui/core/Grid";
 import Chart from 'chart.js';
 
-const numDaySinceBirth = 7602
+
+const numDaySinceBirth = 7603
 const xValue = []
 const Biorythm = () => {
     let label = [];
     let date = new Date();
-    date.setDate(date.getDate()-15)
-    for (let i = 0 ; i <30 ; i++ ){
-        date.setDate(date.getDate()+1);
+    const numDayDisplay = 365;
+    date.setDate(date.getDate() - numDayDisplay/2);
+    for (let i = 0; i < numDayDisplay; i++) {
+        date.setDate(date.getDate() + 1);
         let copyDate = new Date(date.getTime());
-        let day  = copyDate.getUTCDate();
-        let month = copyDate.getMonth()+1;
-        let year = copyDate.getFullYear()+1;
+        let day = copyDate.getUTCDate();
+        let month = copyDate.getUTCMonth() + 1;
 
-        xValue.push({
-            day : day,
-            month : month,
-            year : year
-        })
-        label.push(day + '/' + month );
+        label.push(day + '/' + month);
     }
 
 
     const classes = useStyles();
 
-    const [chartData, setChartData] = useState({})
 
 
-
-    var getDaysInMonth = function(month,year) {
-        // Here January is 1 based
-        //Day 0 is the last day in the previous month
-        return new Date(year, month+1, 0).getDate();
-        // Here January is 0 based
-        // return new Date(year, month+1, 0).getDate();
-    };
-
-
-    function numDiffDay(day,month,year) {
-        const actualDate = new Date();
-        const actualDay = actualDate.getDay();
-        const actualMonth = actualDate.getMonth()+1;
-        if (Math.abs(actualDay - day)<16){
-            return(day-actualDay)
-        }
-        else {
-            if (actualMonth - month == 1|| actualMonth - month == -11){
-                return (-getDaysInMonth(month,year)+Math.abs(actualDay-day))
-            }
-            else if(actualMonth - month== -1|| actualMonth - month == 11){ /*condition non nécessaire mais mieux pour expliquer */
-                return(getDaysInMonth(month,year)-Math.abs(actualDay-day))
-            }
-
-        }
-
-
-
-
-
-    }
 
     var data = {
         labels: label,
+
         datasets: [{
             label: "Physique",
-            function: function(x) {
+            function: function (x) {
 
-                return 100*Math.sin((2*Math.PI*((x%23))/23))
+                return 100 * Math.sin((2 * Math.PI * ((x % 23)) / 23))
 
             },
             borderColor: "rgba(192, 0, 0, 1)",
@@ -80,10 +44,10 @@ const Biorythm = () => {
         },
             {
                 label: "Emotionnel",
-                function: function(x) {
+                function: function (x) {
 
-                    let res=x+numDaySinceBirth-new Date().getDay()
-                    return 100*Math.sin((2*Math.PI*((res%28))/28))
+
+                    return 100 * Math.sin((2 * Math.PI * ((x % 28)) / 28))
                 },
                 borderColor: "rgba(0, 192, 0, 1)",
                 data: [],
@@ -91,9 +55,9 @@ const Biorythm = () => {
             },
             {
                 label: "Intellectuel",
-                function: function(x) {
-                    let res=x+numDaySinceBirth-new Date().getDay()
-                    return 100*Math.sin((2*Math.PI*((res%33))/33))
+                function: function (x) {
+
+                    return 100 * Math.sin((2 * Math.PI * ((x % 33)) / 33))
                 },
                 borderColor: "rgba(0, 0, 192, 1)",
                 data: [],
@@ -103,17 +67,13 @@ const Biorythm = () => {
 
 
 
+
     Chart.pluginService.register({
         beforeInit: function(chart) {
             var data = chart.config.data;
             for (var i = 0; i < data.datasets.length; i++) {
                 for (var j = 0; j < data.labels.length; j++) {
-
-                    let day = xValue[j].day;
-                    let month = xValue[j].month;
-                    let year = xValue[j].year;
-                    let res = numDaySinceBirth + numDiffDay(day, month, year)
-
+                    let res = numDaySinceBirth + j-15
                     var fct = data.datasets[i].function;
                     var y = fct(res);
                     data.datasets[i].data.push(y);
@@ -121,8 +81,6 @@ const Biorythm = () => {
             }
         }
     });
-
-
 
 
     return (
@@ -133,16 +91,29 @@ const Biorythm = () => {
                 </Grid>
                 <Grid item xs={12}>
 
-                    <div style={{ width: "100%"}}>
-                        <Line  data={data} options={{
-                            sampleSize:50000,
+                    <div style={{width: "100%"}}>
+                        <Line data={data} options={{
+                            sampleSize: 50000,
                             responsive: true,
                             title: {text: 'Votre biorythme actuel', display: true},
-                            scales : {
+                            annotation: {
+                                annotations: [{
+                                    drawTime: 'afterDraw', // overrides annotation.drawTime if set
+                                    id: 'a-line-1', // optional
+                                    type: 'line',
+                                    mode: 'vertical',
+                                    scaleID: 'x-axis-0',
+                                    value: '15',
+                                    borderColor: 'black',
+                                    borderWidth: 3,
+                                }]
+                            },
+
+                            scales: {
                                 yAxes: [
                                     {
                                         ticks: {
-                                            stepSize:0.5,
+                                            stepSize: 0.5,
                                             autoSkip: true,
                                             maxTicksLimit: 10,
                                         },
@@ -151,12 +122,12 @@ const Biorythm = () => {
                                         }
                                     }
                                 ],
-                                xAxes:[
+                                xAxes: [
 
                                     {
-                                       gridLines: {
-                                           display: true
-                                       }
+                                        gridLines: {
+                                            display: true
+                                        }
                                     }
                                 ]
                             }
