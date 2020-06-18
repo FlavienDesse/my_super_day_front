e1ad0dfe984614515b41c8e70c75bbca4d58293e
Hyriz <65892443+Hyriz@users.noreply.github.com>
2020-06-18 16:35:45 +0200

    Interface finie

--- src/Components/Traductor/TraductorMain/traductorMain.js
+++ src/Components/Traductor/TraductorMain/traductorMain.js
@@ -3,6 +3,11 @@ import useStyles from './style'
 import Grid from "@material-ui/core/Grid";
 import InputBase from "@material-ui/core/InputBase";
 import Paper from "@material-ui/core/Paper";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import KeyboardTabIcon from '@material-ui/icons/KeyboardTab';
+
 
 function callTrad(props) {
     const requestOptions = {
@@ -28,21 +33,33 @@ function TraductorMain() {
     const [traduction, setTraduction] = React.useState("");
     const [textWanted, setTextWanted] = React.useState("")
     const [lang, setLang] = React.useState("")
+    const language = [
+        {
 
+            show: "Francais",
+            id: "fr"
+        },
+        {
+
+            show: "Allemand",
+            id: "ge"
+        },
+    ];
+    const handleChange = (event) => {
+        setLang(event.target.value);
+    };
 
     return (
         <Paper className={classes.container}>
 
 
-            <Grid container spacing={3}>
+            <Grid container className={classes.grid} direction="row"
+                  justify="flex-start"
+                  alignItems="flex-start"
+            >
                 <Grid item xs={6}>
-
-                </Grid>
-                <Grid item xs={6}>
-                    a
-                </Grid>
-                <Grid item xs={6} className={classes.textWanted}>
                     <InputBase
+                        placeholder={"Entrez votre texte à traduire"}
                         multiline
                         className={classes.textField}
                         defaultValue=""
@@ -50,12 +67,34 @@ function TraductorMain() {
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    a
-                </Grid>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <FormControl variant="filled" className={classes.formControlLang}>
+                                <Select
+                                    className={classes.selectLang}
+                                    onChange={handleChange}
+                                >
+                                    {
+                                        language.map((item, i) => (
 
+                                            <MenuItem value={item.id}> {item.show} </MenuItem>
+
+
+                                        ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} className={classes.answer}>
+
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
+
+
         </Paper>
-    )
+
+)
 }
 
 export default TraductorMain;
