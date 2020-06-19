3dda3a907ca33a4e3f11284267b98a22bbf2b684
VictorienRoussel <65892454+VictorienRoussel@users.noreply.github.com>
2020-06-19 18:21:03 +0200

    agenda1.3

--- src/Components/Agenda/AgendaMain/agenda.js
+++ src/Components/Agenda/AgendaMain/agenda.js
@@ -2,12 +2,23 @@ import React from 'react'
 import BigCalendar from 'react-big-calendar'
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
-import 'moment/locale/fr'  // without this line it didn't work
+import 'moment/locale/fr'
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDateTimePicker
+} from '@material-ui/pickers';
+import MomentUtils from "@date-io/moment";
+import "moment/locale/fr";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import useStyles from "./style";
 
-
-
-function Calendar (){
-
+function Calendar() {
+    const classes = useStyles()
+    const [dateStart, setDateStart] = React.useState(new Date())
+    const [dateEnd, setDateEnd] = React.useState(new Date())
+    const [text, setText] = React.useState("")
 
     moment.locale("fr");
 
@@ -29,24 +40,14 @@ function Calendar (){
     }
 
 
-
     const now = new Date()
     const allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
-    const events= [
-        {
-            id: 0,
-            title: 'All Day Event very long title',
-            start: new Date(),
-            end:new Date(2020,6,19),
-        },
-
-    ]
-
+    const [events, setEvents] = React.useState([])
 
     BigCalendar.momentLocalizer(moment)
 
-    const ColoredDateCellWrapper = ({ children }) =>
+    const ColoredDateCellWrapper = ({children}) =>
         React.cloneElement(React.Children.only(children), {
             style: {
                 backgroundColor: 'pink',
@@ -55,22 +56,96 @@ function Calendar (){
 
 
     return (
-        <div >
+        <div>
             <BigCalendar
                 toolbar={true}
                 events={events}
                 step={60}
                 showMultiDayTimes
-                views={allViews}
+                startAccessor={"start"}
+                endAccessor={"end"}
                 messages={messages}
                 components={{
                     timeSlotWrapper: ColoredDateCellWrapper,
                 }}
-               // view={"week"}
-                views={{ month: true, week: false, day: false, agenda: false, }}
+                defaultView={"week"}
+                //defaultView={"day"}
+                views={{month: true, week: true, day: true, agenda: true,}}
             />
+
+            <Grid container spacing={4} className={classes.gridAddEvent} direction="row" justify="flex-start"
+                  alignItems="center">
+                <Grid item xs={12} md={3}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="addLabel"
+                        label="Veuillez ajouter un libellé"
+                        type="text"
+                        id="addLabel"
+                        onChange={(e) => setText(e.target.value)}
+
+                    />
+
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <MuiPickersUtilsProvider utils={MomentUtils} locale="fr">
+                        <KeyboardDateTimePicker
+                            className={classes.timePickerStart}
+                            minDate={new Date()}
+                            margin="normal"
+                            label="Début de l'évènement"
+                            format="DD/MM/yyyy , h:mm"
+                            onChange={(e) => setDateStart(e)}
+                            value={dateStart}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}>
+
+                        </KeyboardDateTimePicker>
+                    </MuiPickersUtilsProvider>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <MuiPickersUtilsProvider utils={MomentUtils} locale="fr">
+                        <KeyboardDateTimePicker
+                            minDate={dateStart}
+                            margin="normal"
+                            label="Fin de l'évènement"
+                            format="DD/MM/yyyy , h:mm"
+                            onChange={(e) => setDateEnd(e)}
+                            value={dateEnd}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}>
+
+                        </KeyboardDateTimePicker>
+                    </MuiPickersUtilsProvider>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <Button color="primary" variant="contained" className={classes.buttonAdd} onClick={() => {
+                        let newEvent = events.slice()
+
+
+                        newEvent.push({
+                            title: text,
+                            start: dateStart,
+                            end: dateEnd,
+                        })
+
+                        setEvents(newEvent)
+                    }
+
+                    }>
+                        Ajouter
+                    </Button>
+                </Grid>
+            </Grid>
+
+
         </div>
-        )
+    )
 
 
 }
--- src/Components/Agenda/AgendaMain/style.js
+++ src/Components/Agenda/AgendaMain/style.js
@@ -4,6 +4,15 @@ const useStyles = makeStyles((theme) => ({
     agenda:{
         marginTop:'50px',
     },
+    gridAddEvent:{
+      marginTop:'15px',
+    },
+    buttonAdd:{
+      width:'50%',
+    },
+    timePickerStart:{
+        textOverflow:'ellipsis',
+    },
 }));
 
 export default useStyles;
